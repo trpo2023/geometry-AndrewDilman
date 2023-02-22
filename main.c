3cbb6d59f67c209d2d59e5cd2f3ad4903ecca001
AndewDilman <AndrewDilman@users.noreply.github.com>
2023-02-22 09:31:38 +0700

    start making geometry

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#define pi 3.1415
 
 int main()
 {
-    printf("Hello, world! \n");
-    return 0;
+	char k[128]=gets();
+	printf("%s",k);
+	return 0;
+}
+
+int Circle(int x, int y, int r)
+{
+	int p = 2 * r * 3.1415; // perimetr
+	int s = 3.1415 * r * r; // square
 }
