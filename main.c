3c0c6bf45a8d310707f4b0ab982cfe61c1887b54
AndewDilman <AndrewDilman@users.noreply.github.com>
2023-03-22 08:45:19 +0700

    made a working program

--- main.c
+++ main.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
-#define pi 3.1415
+#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-	FILE *myfile;
-	myfile = fopen("circle.txt", "r");
-	char k[128]=gets();
-	printf("%s",k);
-	return 0;
-}
+struct Point {
+  float x;
+  float y;
+};
 
-int CheckInput()
-{
-	
-}
+struct WKTcircle {
+  struct Point point;
+  float rad;
+};
 
-int Circle(int x, int y, int r)
-{
-	int p = 2 * r * 3.1415; // perimetr
-	int s = 3.1415 * r * r; // square
+int main() {
+  FILE *myfile;
+  myfile = fopen("circle.txt", "r");
+
+  if(myfile==0){
+    printf("no such file in derictory \n");
+    return 0;
+  }
+
+  char *type;
+  char line[100];
+  char line1[100];
+
+  struct WKTcircle c1;
+  while ((fgets(line, 100, myfile)) != 0) {
+    strcpy(line1, line);
+    //printf("%s \n ", line1);
+    type = strtok(line1, "(");
+    //printf("%s, %s \n ", line1, type);
+    if (strcmp(type, "circle") == 0) {
+      if ((sscanf(line, "circle(%f %f, %f)", &c1.point.x, &c1.point.y, &c1.rad)) == 3)
+        printf("circle(%f %f  %f) \n", c1.point.x, c1.point.y, c1.rad);
+      else
+        printf("incorrect data of points of circle. Expected NUM NUM, NUM \n");
+    }
+    else
+      printf("Incorrect type of figure format. Expected: \"Circle\" \n");
+  }
+  return 0;
 }
